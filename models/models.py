9b2b4fa07769a912bbadb20423ae394200a5d569
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-29 04:37:25 +0300

    Updated an import

--- models/models.py
+++ models/models.py
@@ -1,8 +1,8 @@
 """The file that holds the models logic"""
 from datetime import datetime
 
-from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, Integer, String,
-                        relationship)
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from database.db import Base
 
