e3e4bc29fcb229d14a7947e689119fe719f5e47a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-29 04:34:57 +0300

    Created the models file

---
+++ models/models.py
@@ -0,0 +1,36 @@
+"""The file that holds the models logic"""
+from datetime import datetime
+
+from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, Integer, String,
+                        relationship)
+
+from database.db import Base
+
+
+class User(Base):
+    """The model for the user data
+
+    Args:
+        Base (Db Base): The database base class
+    """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(255))
+    is_active = Column(Boolean, default=True)
+
+    articles = relationship(
+        "Article", back_populates="author", cascade="all,delete")
+
+
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), index=True)
+    body = Column(String(255))
+    created_at = Column(DateTime(), default=datetime.now())
+    updated_at = Column(DateTime(), onupdate=datetime.now())
+    author_id = Column(Integer, ForeignKey("users.id"),
+                       nullable=False, ondelete="CASCADE")
